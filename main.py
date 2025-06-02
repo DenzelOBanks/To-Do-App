b9c5d0cbb45050ab47c4667626b68d5dbcbc835b
Denzel <Denzelobanks@gmail.com>
2025-06-02 15:10:24 +0100

    Refactor To-Do app: update routes, templates, and database model

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-from flask import Flask, render_template, request, session, flash
+from flask import Flask, render_template,request,url_for,redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = ""
+app.secret_key = "To-do"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class todo(db.Model):
      id = db.Column(db.Integer, primary_key=True)
      title = db.Column(db.String(100))
      complete = db.Column(db.Boolean)
 
 @app.route("/")
-def home():
-    return render_template("home.html")
+def index():
+    todo_list = todo.query.all()
+    return render_template("base.html", todo_list=todo_list)
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
-     db.create_all()
-     app.run(debug=True)
+     with app.app_context():
+      db.create_all()
+      app.run(debug=True)
